3cd77d4b95745e4117808325f0d552e70cfebb56
Riad Alhaj <ryadelhaj@gmail.com>
2024-10-02 00:54:40 +0200

    finsh the age function

--- aboutMe.js
+++ aboutMe.js
@@ -2,7 +2,13 @@ function displayName (callback) {
   setTimeout(() =>{
     console.log("Name: Riad Alhaj");
     callback();
-
   },1000);
+}
+
+function displayAge(callback) {
+  setTimeout(() => {
+    console.log("Age: 31");
+    callback();
+  }, 2000); 
 }
 
